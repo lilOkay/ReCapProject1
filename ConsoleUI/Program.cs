8de5c97007c644d5a7ed02d73b4e49ba416ee46c
lilOkay <gncokay@gmail.com>
2021-02-12 21:26:51 +0300

    12 şubat ödev

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,40 +12,72 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //CarTest();
-            //BrandTest();
-            //ColorTest();
-            //CarDetailTest();
+            //gun9();
 
-            RentalManager rentalMenager = new RentalManager(new EfRentalDal());
-            var result = rentalMenager.GetAll();
-            if (result.Success == true)
+            UserManager userManager = new UserManager(new EfUserDal());
+            userManager.Add(new User()
+            {FirstName = "Okay ",LastName = "Genc",Email = "ogenc192@gmail.com",Password = "asdfg"});
+
+            foreach (var user in userManager.GetAll().Data)
             {
-                foreach (var rent in result.Data)
-                {
-                    Console.WriteLine(rent.CarId + "--" + rent.CustomerId + "--" + rent.RentDate + "--" + rent.ReturnDate);
-                }
+                Console.WriteLine($"{user.Id}, {user.FirstName} {user.LastName}, {user.Email}");
             }
+
+            Console.WriteLine("---------Add Customer---------");
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+            customerManager.Add(new Customer()
+            {
+                UserId = 1,
+                CompanyName = "Company Name 1"
+            });
+
+            foreach (var customer in customerManager.GetAll().Data)
+            {
+                Console.WriteLine($"{customer.UserId}, {customer.CompanyName}");
+            }
+
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            var add = rentalManager.Add(new Rental()
+            {
+                CarId = 1,
+                CustomerId = 1,
+                RentDate = DateTime.Now,
+            });
+
+         
 
         }
 
-        private static void CarDetailTest()
+        private static void gun9()
         {
             CarManager carManager = new CarManager(new EfCarDal());
+            carManager.Add(new Car()
+            {
+                BrandId = 1,
+                Description = "Core Sonrası Araba 3",
+                ColorId = 2,
+                DailyPrice = 1000,
+                ModelYear = 2019
+            });
 
-            var result = carManager.GetCarDetails();
-            carManager.Add(new Car { BrandId = 5, ColorId = 3, DailyPrice = 750, Description = "Sport Araba", ModelYear = 2013 });
-            if (result.Succes == true)
+            Console.WriteLine("\n=============GetAll=============");
+            foreach (var car in carManager.GetAll().Data)
             {
-                foreach (var details in result.Data)
-                {
-                    Console.WriteLine(details.CarId + "--" + details.BrandName + "--" + details.Description + "--" + details.ColorName + "--" + details.DailyPrice + "$");
-                }
+                Console.WriteLine($"CarId: {car.CarId}   BrandId: {car.BrandId}   ColorId: {car.ColorId}   " +
+                                  $"Description: {car.Description}   DailyPrice: {car.DailyPrice}   ModelYear: {car.ModelYear}");
             }
-            else
+
+            Console.WriteLine("\n=============GetCarsDetailDTO=============");
+            foreach (var car in carManager.GetCarDetails().Data)
             {
-                Console.WriteLine(result.Message);
+                Console.WriteLine($"{car.Description}   {car.ColorName}   {car.BrandName}   {car.DailyPrice}");
             }
+
+            Console.WriteLine("\n=============GetCarById=============");
+            var id4Car = carManager.GetById(4);
+            Console.WriteLine(
+                $"CarId: {id4Car.Data.CarId}   BrandId: {id4Car.Data.BrandId}   ColorId: {id4Car.Data.ColorId}   " +
+                $"Description: {id4Car.Data.Description}   DailyPrice: {id4Car.Data.DailyPrice}   ModelYear: {id4Car.Data.ModelYear}");
         }
     }  
 }
